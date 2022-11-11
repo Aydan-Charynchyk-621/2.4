34ac38647ad2f2c425396a3d134789505186f3e6
Чарынчык Айдан Валерьевич <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2022-11-12 00:44:08 +0700

    Add files via upload

---
+++ dart_application_1.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+//
+//
+//Задние 1
+//
+//
+//int min = 1;
+//int max = 11;
+//List<int> nums = List.generate(10, (w) => min + Random().nextInt(max - min));
+//nums.sort();
+//print(nums);
+//print(nums.first);
+//}
+//
+//
+//Задание 2
+//
+//List nums = ['Массив чисел'];
+//int proizv = 1;
+//int summa = 0;
+//int num = 0;
+//for(int i = 0; i < nums.length; i++) {
+//print("Числа- ");
+//int a = int.parse(stdin.readLineSync()!);
+//summa += a;
+//if(a != 0) {
+//num++;
+//proizv *= a;
+//nums.add(a);
+//print(nums);
+//} else {
+//break;
+//}
+//}
+//print("Произведение-$proizv");
+//print("Сумма-$summa");
+//print("Среднее арифметическое-$num");
+//}
+//
+//
+//Задание 3
+//
+//
+//print([7,17,621,1007,2500,330000,15000000].reduce(min));
+//print([7,17,621,1007,2500,330000,15000000].reduce(max));
+//}
+//
+//
+//Задание 4
+//
+//
+//stdout.write('Введите начало диапазона-');
+//int min = int.parse(stdin.readLineSync()!);
+//stdout.write('Введите конец диапазона-');
+//int max = int.parse(stdin.readLineSync()!);
+//List<int> nums = List.generate(11, (w) => min + Random().nextInt(max - min));
+//for(int i = 0; i < 1; i++)
+//print(nums);
+//}
+//
+//
+//Задание 5
+//
+//stdout.write("Введите слово-");
+//String a = stdin.readLineSync()!;
+//print(a.length - 4);
+//print("Start $a End");
+//}
+}
